eac269418cf03c502a68b3691c62f566d08525f5
Vathila De Silva <86126854+Vat2002@users.noreply.github.com>
2024-02-20 12:27:16 +0530

    Updated Navigation links
    update links to other pages

--- app.py
+++ app.py
@@ -10,5 +10,12 @@ def home_screen():
     return render_template('home.html')
 
 
+@app.route('/generate_caption.html')
+# def hello_world():  # put application's code here
+#     return 'Hello World!'
+def generate_caption_page():
+    return render_template('generate_caption.html')
+
+
 if __name__ == '__main__':
     app.run()
